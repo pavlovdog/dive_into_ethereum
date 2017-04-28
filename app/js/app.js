0f78b14aab3e9da9ae1f3cba6d7bb69f706bedba
pavlovdog <pavlovdog.moscow@gmail.com>
2017-04-28 12:54:13 +0300

    App finished

--- app/js/app.js
+++ app/js/app.js
@@ -1,38 +1,111 @@
+// ==========================
+// ===== Draw skills ========
+// ==========================
+google.charts.load('current', {packages: ['corechart', 'bar']});
+
+function drawSkills() {
+  data = [['Skill', 'Level']];
+  for (var skill of app.skills) {
+    data.push([skill.name, skill.level]);
+  }
+  var data = google.visualization.arrayToDataTable(data);
+
+  var options = {
+    chartArea: {width: '50%'},
+    hAxis: {
+      minValue: 0
+    },
+    legend : 'none',
+    chartArea:{left:'10%',top:'5%',width:'90%',height:'80%'},
+    colors : ['#1E88E5']
+  };
+
+  var chart = new google.visualization.BarChart(document.getElementById('skills'));
+  chart.draw(data, options);
+}
+
+
+var app = new Vue({
+  el : "#app",
+  data : {
+    basic_data : {},
+    projects : [],
+    educations : [],
+    skills : [],
+    publications : []
+  }
+})
+
 function startApp() {
+  // var currentTime = new Date().getTime();
+  // while (currentTime + 2000 >= new Date().getTime()) {}
+
   var address = {
-    "3" : "0x0CbF3F0EdFe8EDc4D34968a0c5c54b20C2E3C85B" // Ropsten
+    "3" : "0xcfc13ef85d5c37c0b54da991289f44450c3f30d6" // Ropsten
   }
 
-  var current_network = web3.version.network;
-  var contract = web3.eth.contract(abi).at(address[current_network]);
+  // var current_network = web3.version.network;
+  // var contract = web3.eth.contract(abi).at(address[current_network]);
 
-
+  var contract = web3.eth.contract(abi).at(address["3"]);
   console.log("Contract initialized successfully")
-  var basic_keys = ["name", "age", "home", "email", "tel"]
 
-  contract.getBasicData("name", function(error, data) {
-    console.log(error, data);
-  });
+  // Get basic data
+  var basic_items = ["name", "address", "email", "tel", "github", "stackexchange"];
+  basic_items.map(function(item) {
+    contract.getBasicData(item, function(error, data) {
+      console.log(item, data);
+      app.basic_data[item] = data;
+    });
+  })
 
-  contract.getBasicData("age", function(error, data) {
-    console.log(error, data);
-  });
-
-  contract.getBasicData("agasde", function(error, data) {
-    console.log(error, data);
-  });
-
+  // Get skills
   contract.getSize("skills", function(error, data) {
-    var skills_size = data.c[0];
-    for (var i = 0; i < skills_size; ++i) {
-      contract.skills(i, function(error, data) {
-        console.log(i, data);
+    var item_size = data["c"][0];
+    range(0, item_size - 1).map(function(item) {
+      contract.skills(item, function(error, data) {
+        console.log(item, data);
+        app.skills.push({"name" : data[0], "level" : data[1]["c"][0]})
+        if (item == item_size - 1) { drawSkills(); }
+      })
+    })
+  })
+
+  // Get projects
+  contract.getSize("projects", function(error, data) {
+    var item_size = data["c"][0];
+    for (var i = 0; i < item_size; ++i) {
+      contract.projects(i, function(error, data) {
+        console.log(data);
+        app.projects.push({"name" : data[0], "desc" : data[1], "link" : data[2]})
       })
     }
   })
 
-  contract.skills(0, function(error, data) {
-    console.log(error, data);
+  // Get educations
+  contract.getSize("educations", function(error, data) {
+    var item_size = data["c"][0];
+    for (var i = 0; i < item_size; ++i) {
+      contract.educations(i, function(error, data) {
+        console.log(data);
+        app.educations.push({
+          "name" : data[0],
+          "speciality" : data[1],
+          "year_start" : data[2]["c"][0],
+          "year_finish" : data[3]["c"][0]
+        })
+      })
+    }
   })
 
+  // Get publications
+  contract.getSize("publications", function(error, data) {
+    var item_size = data["c"][0];
+    for (var i = 0; i < item_size; ++i) {
+      contract.publications(i, function(error, data) {
+        console.log(data);
+        app.publications.push({"name" : data[0], "link" : data[1], "language" : data[2]});
+      })
+    }
+  })
 }
--- app/js/onload.js
+++ app/js/onload.js
@@ -1,17 +1,25 @@
-window.addEventListener('load', function() {
+var range = (start, end) => [...Array(end - start + 1)].map((_, i) => start + i);
 
+window.addEventListener('load', function() {
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
     // Use Mist/MetaMask's provider
     console.log("Web3 detected!");
     window.web3 = new Web3(web3.currentProvider);
+    // Now you can start your app & access web3 freely:
+    startApp();
   } else {
-    console.log('No web3? You should consider trying MetaMask!')
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    swal({
+      title : 'Oops...',
+      html :  '<h4>My usual CV (.pdf) is available <a href="https://pavlovdog.github.io/potekhin_sergey.pdf">here</a></h4>' +
+              '<p align="left">This web page was build with <a href="https://www.ethereum.org/" target="_blank">Ethereum platform</a> and at seems like your browser doesn\'t support it yet!</p>' +
+              '<p align="left">If you still want to use this page, please, make sure that:</p>' +
+              '<ul align="left">' +
+              '<li>You\'re using <a href="https://www.google.com/chrome/" target="_blank">Chrome browser</a></li>' +
+              '<li>You have installed <a href="https://metamask.io/" target="_blank">MetaMask plugin</a></li>' +
+              '<li><code>Ropsten Test Net</code> is enabled (top-left corner in the Metamask settings)</li>' +
+              '</ul>',
+      type : 'error',
+    })
   }
-
-  // Now you can start your app & access web3 freely:
-  startApp()
-
 })
